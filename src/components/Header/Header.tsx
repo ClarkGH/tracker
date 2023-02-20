0a206fe1fbb127e523442c5b61eb8f19030d2fe3
clarkgh <clarkhinchcliff@gmail.com>
2023-02-20 18:18:17 -0500

    remove boilerplate code

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import {signIn, signOut, useSession} from "next-auth/react";
+
+export const Header = () => {
+    const { data: sessionData } = useSession();
+
+    return (
+        <div aria-label="header">
+            <div>{sessionData?.user?.name ? `${sessionData.user.name}, here are your notes` : ''}</div>
+            <div>{sessionData?.user ? (
+                <button onClick={() => void signOut()}>
+                    <img
+                        src={sessionData?.user?.image ?? ''}
+                        alt={sessionData?.user?.name ?? ''}
+                    />
+                </button>
+            ) : (
+                <button onClick={() => void signIn()}>
+                    Sign In
+                </button>
+            )}</div>
+        </div>
+    )
+}
--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,4 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
 import { sectionRouter } from "~/server/api/routers/section";
 
 /**
@@ -8,7 +7,6 @@ import { sectionRouter } from "~/server/api/routers/section";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
   section: sectionRouter,
 });
 
